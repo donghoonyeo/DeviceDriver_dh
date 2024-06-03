af35eca9cbaa971dc33d6e00a32c1d2aeab8c2eb
donghoonyeo <www1w1w11@gmail.com>
2024-06-03 11:44:19 +0900

    read Test code + feature 구현 및 리펙토링

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -1,16 +1,53 @@
 #include "DeviceDriver.h"
+#include <windows.h>
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ReadFailException : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Read Fail Exception";
+    }
+};
+
+class WriteFailException : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Write Fail Exception";
+    }
+};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
+bool DeviceDriver::isReadOk(int address, int firstReadVal)
+{
+    for (int i = 0; i < 4; i++) {
+        //Sleep(200);
+        if (firstReadVal != (int)m_hardware->read(address))
+            return false;
+    }
+    return true;
+}
+
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int result = (int)m_hardware->read(address);
+    if(isReadOk(address, result) == false)
+        throw ReadFailException();
+
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
 {
     // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+    m_hardware->write(address, (int)data);
 }
--- Sample-Test1/DeviceDriver_test.cpp
+++ Sample-Test1/DeviceDriver_test.cpp
@@ -2,7 +2,35 @@
 #include "gmock/gmock.h"
 #include "../Project1/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+
+class FlashMock : public FlashMemoryDevice {
+public :
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class FlashMemoryTester : public Test {
+public:
+	FlashMock flashDevice;
+	DeviceDriver testDevice{ &flashDevice };
+};
+
+TEST_F(FlashMemoryTester, Read5TimesPass) {
+	EXPECT_CALL(flashDevice, read)
+		.Times(5)
+		.WillRepeatedly(Return(52));
+
+	EXPECT_THAT(testDevice.read(0x1000), Eq(52));
+}
+
+TEST_F(FlashMemoryTester, Read5TimesFail) {
+	EXPECT_CALL(flashDevice, read)
+		.WillOnce(Return(52))
+		.WillOnce(Return(52))
+		.WillOnce(Return(52))
+		.WillOnce(Return(52))
+		.WillRepeatedly(Return(33));
+
+	EXPECT_THROW(testDevice.read(0x1000), ReadFailException);
 }
